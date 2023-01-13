8f1b3a3cfe1f960202feea4420c47548d3f1f2a9
dnrgus1127 <dnrgus1127@naver.com>
2023-01-14 01:56:52 +0900

    FEAT : Blog Service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import Header from "./components/Header";
+
+const Body = styled.div`
+  padding: 0 var(--gap);
+  padding-top: var(--header);
+`;
+
+const Footer = styled.footer`
+  height: 15vh;
+  width: calc(100% - var(--gap) * 2);
+  padding: 0 var(--gap);
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Body>바디영역</Body>
+      <Footer>푸터영역</Footer>
     </div>
   );
 }
