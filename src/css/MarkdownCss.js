a0fd8a5b008d6748bc504991a551d16472a84691
dnrgus1127 <dnrgus1127@naver.com>
2023-01-20 02:42:36 +0900

    FEAT : ringblog markdown CSS styled-components

---
+++ src/css/MarkdownCss.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const MarkdownCss = styled.div`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 3rem 0;
+  }
+
+  hr {
+    border: 1px solid ${({ theme }) => theme.lineColor};
+    margin: 3rem 0;
+  }
+
+  ul {
+    margin: 2rem;
+  }
+  & > ul {
+    list-style-type: square;
+    list-style-position: inside;
+  }
+  & > ul > li {
+    margin: 2rem 0;
+  }
+  & > ul > li > ul {
+    list-style-type: circle;
+  }
+`;
+
+export { MarkdownCss };
