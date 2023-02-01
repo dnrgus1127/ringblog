5772cc36c25f9927f5780218944669c9fb792a13
dnrgus1127 <dnrgus1127@naver.com>
2023-02-01 19:52:21 +0900

    FEAT : ReactMarkdown 제공 컴포넌트

---
+++ src/components/markdown/CustomMD.js
@@ -0,0 +1,40 @@
+import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { stackoverflowDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import remarkGfm from "remark-gfm";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+// TODO - 다크모드 아닐 때 SyntaxHighlighter style 추가 필요
+export default function CustomMD({ children }) {
+  return (
+    <ReactMarkdown
+      children={children}
+      remarkPlugins={[remarkGfm]}
+      // Code Hilghter
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            // ? 언어에 따라서 코드 구문 스타일 지정
+            <SyntaxHighlighter
+              language={match[1]}
+              style={stackoverflowDark}
+              customStyle={{ padding: "1.5rem 1rem" }}
+            >
+              {children}
+            </SyntaxHighlighter>
+          ) : (
+            // ? Code 언어 선택 안했을 때 코드 구문 스타일 지정
+            <SyntaxHighlighter
+              style={stackoverflowDark}
+              customStyle={{ padding: "1.5rem 1rem" }}
+            >
+              {children}
+            </SyntaxHighlighter>
+            // <code>{children}</code>
+          );
+        },
+      }}
+    ></ReactMarkdown>
+  );
+}
