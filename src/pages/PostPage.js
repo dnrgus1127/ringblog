8a16676aef78faaf36e29aa575c582420047c087
dnrgus1127 <dnrgus1127@naver.com>
2023-02-03 00:12:00 +0900

    Refactor : Fetch Component 사용

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,28 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
 import { useQuery } from "../functions/urlQuery";
 import PostContents from "../components/PostContents";
-import { getPostByIndex } from "../functions/fetch";
+import { Fetch } from "../components/Fetch";
 
 const Body = styled.div`
   padding-top: calc(var(--header) * 1.5);
+  width: calc(var(--width) * 0.65);
+  margin: 0 auto;
 `;
 
 export default function PostPage({ theme, toggleTheme }) {
   let query = useQuery();
   const index = query.get("index");
-  const [post, setPost] = useState({});
-  useEffect(() => {
-    getPostByIndex(index).then(setPost);
-  }, [index]);
+  const uri = `/posts/${index}`;
 
   return (
     <React.Fragment>
       <Header theme={theme} toggleTheme={toggleTheme} />
       <Body>
-        <PostContents post={post} />
+        <Fetch uri={uri} renderSucsecc={fetchSucces}></Fetch>
       </Body>
     </React.Fragment>
   );
+}
+
+function fetchSucces({ data }) {
+  return <PostContents post={data} />;
 }
