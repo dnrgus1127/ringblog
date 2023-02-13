853c47438786a296f4450496e8b38d214dd9ea2e
dnrgus1127 <dnrgus1127@naver.com>
2023-02-13 23:56:01 +0900

    FEAT : Header dir 구조 변경

---
+++ src/components/Header/HideMenu.js
@@ -0,0 +1,91 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Context } from "../../functions/Login/LoginProvider";
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 80vh;
+  background-color: ${({ theme }) => theme.bgElement};
+  width: 100%;
+  transform: translateY(-100%);
+  transition: 0.3s all ease-out;
+  padding: 0 var(--gap);
+  padding-top: calc(var(--header) * 0.75);
+  z-index: 9998;
+
+  .wrap {
+    width: var(--width);
+    margin: 0 auto;
+  }
+`;
+const SiteMap = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 var(--gap);
+`;
+
+const UserMenu = styled.div`
+  display: flex;
+  justify-content: end;
+  /* margin-right: 2rem; */
+  /* margin-top: 2rem; */
+
+  & > .border {
+    padding: 1.5rem;
+  }
+
+  h2 {
+    margin-bottom: 2rem;
+    color: ${({ theme }) => theme.btnColor};
+  }
+  ul {
+    list-style: none;
+  }
+  li {
+    cursor: pointer;
+    margin: 1rem;
+    font-weight: 600;
+  }
+
+  li:hover {
+    color: ${({ theme }) => theme.warning};
+  }
+`;
+
+export default function HideMenu({ trigger }) {
+  const { loggedUser, loggedIn } = useContext(Context);
+
+  const userInfo = (
+    <UserMenu>
+      <div className='border'>
+        <h2>{loggedUser.username}님</h2>
+        <ul>
+          <Link to={`/UserBlog?writer=${loggedUser.userId}`}>
+            <li>내 블로그</li>
+          </Link>
+          <li>읽기 목록</li>
+          <li>설정</li>
+          <li>로그아웃</li>
+        </ul>
+      </div>
+    </UserMenu>
+  );
+
+  return (
+    <Container
+      style={
+        trigger
+          ? { transform: "translateY(0%)" }
+          : { transform: "translateY(-100%)" }
+      }
+    >
+      <div className='wrap'>
+        {loggedIn ? userInfo : null}
+        <SiteMap></SiteMap>
+      </div>
+    </Container>
+  );
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import BlogList from "../components/posts/BlogList";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 
 const Body = styled.div`
   padding-top: calc(var(--header) * 1.5);
