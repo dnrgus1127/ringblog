fcf6f516dcc0561cac985b9c1c611da82056e041
dnrgus1127 <dnrgus1127@naver.com>
2023-03-25 01:45:39 +0900

    FEAT (REDUX) : reduxToolkit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import writeReducer from "./redux/writeReducer";
+
+let store = configureStore({
+  reducer: {
+    write: writeReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //   <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
   // </React.StrictMode>
 );
