8c433b0ef01d3589879a29a8a35e70b7f385eac1
dnrgus1127 <dnrgus1127@naver.com>
2023-03-29 01:53:22 +0900

    FEAT (Button) : 버튼 컴포넌트 리뉴얼 작업중

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,16 +9,9 @@ const BtnCss = styled.button`
   font-family: inherit;
   font-size: 1.6rem;
   font-weight: 800;
-  /* color: ${(props) => (props.color ? props.color : "#000000")}; */
-  /* background-color: ${(props) => (props.bg ? props.bg : "none")}; */
+
   transition: 0.3s all ease-out;
   cursor: pointer;
-  /* &:hover {
-    transform: scale(1.1);
-  } */
-  @media (max-width: 640px) {
-    font-size: 1.6rem;
-  }
 `;
 
 const ColorButton = styled(BtnCss)`
---
+++ src/components/common/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+  font-family: inherit;
+  font-size: 1.6rem;
+  font-weight: 800;
+  padding: 1rem 2rem;
+  border-radius: 4px;
+  ${(props) =>
+    props.size === "small" &&
+    css`
+      font-size: 1.4rem;
+      padding: 0.6rem 1.2rem;
+      border-radius: 2px;
+    `}
+  ${(props) =>
+    props.size === "medium" &&
+    css`
+      font-size: 1.6rem;
+      padding: 0.8rem 1.6rem;
+    `}
+  ${(props) =>
+    props.size === "large" &&
+    css`
+      font-size: 1.8rem;
+      padding: 1rem 2rem;
+    `}
+`;
+
+const Confirm = styled(Button)`
+  background-color: ${({ theme }) => theme.btnColor};
+  color: ${({ theme }) => theme.oppositeColor};
+`;
+
+export const CancelButton = ({ onClick, children, size = "medium" }) => {
+  return (
+    <Button onClick={onClick} size={size}>
+      {children}
+    </Button>
+  );
+};
+
+export const ConfirmButton = ({ onClick, children, size = "medium" }) => {
+  return (
+    <Confirm onClick={onClick} size={size}>
+      {children}
+    </Confirm>
+  );
+};
