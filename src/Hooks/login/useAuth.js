3f7afb130860f5688db76946f44027009610f8dd
dnrgus1127 <dnrgus1127@naver.com>
2023-04-12 17:37:15 +0900

    FEAT (Auth) : 로그인 커스텀 훅

--- src/Hooks/login/useAuth.js
+++ src/Hooks/login/useAuth.js
@@ -2,6 +2,8 @@ import { useMutation } from "react-query";
 import { useDispatch } from "react-redux";
 import { loginActions } from "../../redux/loginState";
 
+// TODO 로그인, 로그아웃 관련된 커스텀 훅
+// ! 현재 로그아웃만 구현
 export default function useAuth() {
   const dispatch = useDispatch();
 
