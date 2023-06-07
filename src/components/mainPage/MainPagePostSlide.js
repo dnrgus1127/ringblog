3cab5004ae60351eb8cc97297327a9feb5e0353f
dnrgus1127 <dnrgus1127@naver.com>
2023-06-08 00:02:13 +0900

    FEAT (PostSlide) : 슬라이드 자동 넘기기 기능 추가

--- src/components/mainPage/MainPagePostSlide.js
+++ src/components/mainPage/MainPagePostSlide.js
@@ -9,7 +9,6 @@ const PostSlideWindow = styled.div`
   position: relative;
 `;
 
-
 const PostSliderBlock = styled.div`
   width: 100%;
   height: 100%;
@@ -35,19 +34,19 @@ const BtnBlock = styled.div`
   height: 100%;
   align-items: center;
   justify-content: space-between;
-  padding : 0 2rem;
-`
+  padding: 0 2rem;
+`;
 
 const SlideBtn = styled.button`
   svg {
-    fill :white;
-    filter: drop-shadow(0px 0px 5px rgba(0,0,0,.5));
+    fill: white;
+    filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.5));
 
     width: 3rem;
     height: 3rem;
   }
   z-index: 500;
-`
+`;
 const SlideIndicatorBox = styled.div`
   display: flex;
   justify-content: center;
@@ -57,7 +56,7 @@ const SlideIndicatorBox = styled.div`
   svg {
     width: 1.5rem;
     height: 1.5rem;
-    fill : ${({ theme }) => theme.pointColor};
+    fill: ${({ theme }) => theme.pointColor};
   }
   circle {
     display: none;
@@ -67,10 +66,9 @@ const SlideIndicatorBox = styled.div`
       display: initial;
     }
   }
-`
+`;
 
 const indicatorList = ["", "", "", "", "", ""];
-
 
 export default function MainPagePostSlide({ data }) {
   const [index, setSlideIndex] = useState(1);
@@ -78,6 +76,16 @@ export default function MainPagePostSlide({ data }) {
 
   const lastIdx = data.length;
   const startIdx = 1;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((prev) => prev + 1);
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   // 무한 슬라이딩 가능하도록
   useEffect(() => {
@@ -123,14 +131,46 @@ export default function MainPagePostSlide({ data }) {
           )}
         </PostSliderBlock>
         <BtnBlock>
-          <SlideBtn onClick={preItem}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" /></svg></SlideBtn>
-          <SlideBtn onClick={nextItem}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg></SlideBtn>
+          <SlideBtn onClick={preItem}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+            >
+              <path d='M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z' />
+            </svg>
+          </SlideBtn>
+          <SlideBtn onClick={nextItem}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+            >
+              <path d='M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z' />
+            </svg>
+          </SlideBtn>
         </BtnBlock>
       </PostSlideWindow>
       <SlideIndicatorBox>
-        {indicatorList.map((i, idx) => <svg className={idx + 1 === index && "selected"} clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z" fillRule="nonzero" /><circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" /></svg>)}
-
-
+        {indicatorList.map((i, idx) => (
+          <svg
+            className={idx + 1 === index && "selected"}
+            clipRule='evenodd'
+            fillRule='evenodd'
+            strokeLinejoin='round'
+            strokeMiterlimit='2'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z'
+              fillRule='nonzero'
+            />
+            <circle cx='11.998' cy='11.998' fillRule='nonzero' r='9.998' />
+          </svg>
+        ))}
       </SlideIndicatorBox>
     </div>
   );
