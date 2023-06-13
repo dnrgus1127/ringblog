090fc8083e92976ee01d9b91f28cd454387bbbb3
dnrgus1127 <dnrgus1127@naver.com>
2023-06-13 19:15:00 +0900

    Style :  코드 포맷팅

--- src/components/UserBlog/UserBlogTemplate.js
+++ src/components/UserBlog/UserBlogTemplate.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Header from '../Header/Header'
-import styled from 'styled-components'
-import media from '../../lib/style/media'
+import React from "react";
+import Header from "../Header/Header";
+import styled from "styled-components";
+import media from "../../lib/style/media";
 
 const UserBlogTemplateBlock = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const UserBlogContentsBlock = styled.div`
   width: var(--width);
   background-color: ${({ theme }) => theme.bgColor};
-  margin-top: calc(var(--header) *2);
+  margin-top: calc(var(--header) * 2);
 
   h1 {
     margin-bottom: 4rem;
@@ -19,44 +19,47 @@ const UserBlogContentsBlock = styled.div`
   img {
     width: 100%;
   }
-`
+`;
 
 const UserBlogInfomation = styled.div`
-    display: -webkit-flex;
-    display: flex;
-    justify-content: space-between;
-    align-items: start;
-    margin-bottom: 5rem;
-    #blogIntrodution {
-      display: -webkit-box;
-        width: 45%;
-        max-height: 10rem;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        word-wrap: break-word;
-        word-break: keep-all;
-        -webkit-line-clamp : 4;
-        -webkit-box-orient: vertical;
+  display: -webkit-flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  margin-bottom: 5rem;
+  #blogIntrodution {
+    display: -webkit-box;
+    width: 45%;
+    max-height: 10rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-wrap: break-word;
+    word-break: keep-all;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+  }
+  #github-calender {
+    width: 45%;
+  }
 
+  ${media.large} {
+    flex-direction: column;
+    gap: 3rem;
+    #blogIntrodution {
+      width: 100%;
     }
     #github-calender {
-        width: 45%;
+      width: 100%;
     }
+  }
+`;
 
-    ${media.large} {
-      flex-direction: column;
-      gap : 3rem;
-      #blogIntrodution {
-        width: 100%;
-      }
-      #github-calender {
-        width: 100%;
-      }
-    }
-
-`
-
-export default function UserBlogTemplate({ blogerName = "", blogIntro = "", blogerId = "", children }) {
+export default function UserBlogTemplate({
+  blogerName = "",
+  blogIntro = "",
+  blogerId = "",
+  children,
+}) {
   return (
     <UserBlogTemplateBlock>
       <Header />
@@ -64,10 +67,14 @@ export default function UserBlogTemplate({ blogerName = "", blogIntro = "", blog
         <h1>{blogerName}</h1>
         <UserBlogInfomation>
           <section id='blogIntrodution'>{blogIntro}</section>
-          <img id='github-calender' src={`https://ghchart.rshah.org/000000/${blogerId}`} alt="github잔디" />
+          <img
+            id='github-calender'
+            src={`https://ghchart.rshah.org/736ac1/${blogerId}`}
+            alt='github잔디'
+          />
         </UserBlogInfomation>
         {children}
       </UserBlogContentsBlock>
     </UserBlogTemplateBlock>
-  )
+  );
 }
