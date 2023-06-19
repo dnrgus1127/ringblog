b98da9c4f18387f71058f688633517e4c47885c4
dnrgus1127 <dnrgus1127@naver.com>
2023-06-19 23:41:20 +0900

    FEAT  (Button ) : 비활성화 css 추가

--- src/components/common/button/Button.js
+++ src/components/common/button/Button.js
@@ -33,7 +33,7 @@ const Button = styled.button`
       font-size: 1.8rem;
       padding: 1rem 2rem;
     `}
-  &:disabled {
+  &:disabled, &:disabled:hover {
     background-color: ${({ theme }) => theme.greyColor};
   }
 `;
@@ -49,8 +49,12 @@ export const CancelButton = ({ onClick, children, size = "medium" }) => {
 
 //  확인 버튼
 const Confirm = styled(Button)`
-  background-color: ${({ theme }) => theme.btnColor};
+  background-color: ${({ theme }) => theme.pointColor};
   color: ${({ theme }) => theme.oppositeColor};
+
+  :hover {
+    background-color: ${({ theme }) => theme.btnHover};
+  }
 `;
 
 export const ConfirmButton = ({
@@ -59,9 +63,16 @@ export const ConfirmButton = ({
   size = "medium",
   type,
   className,
+  disabled,
 }) => {
   return (
-    <Confirm onClick={onClick} size={size} type={type} className={className}>
+    <Confirm
+      onClick={onClick}
+      size={size}
+      type={type}
+      className={className}
+      disabled={disabled}
+    >
       {children}
     </Confirm>
   );
@@ -106,5 +117,33 @@ export const ColorChangingButton = ({
     >
       {children}
     </ColorChanging>
+  );
+};
+
+const Close = styled(Confirm)`
+  padding: 1rem;
+
+  svg {
+    display: block;
+    margin: 0 auto;
+    width: 2rem;
+    height: 2rem;
+  }
+`;
+
+export const CloseButton = ({ onClick }) => {
+  return (
+    <Close onClick={onClick}>
+      <svg
+        clipRule='evenodd'
+        fillRule='evenodd'
+        strokeLinejoin='round'
+        strokeMiterlimit='2'
+        viewBox='0 0 24 24'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path d='m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z' />
+      </svg>
+    </Close>
   );
 };
