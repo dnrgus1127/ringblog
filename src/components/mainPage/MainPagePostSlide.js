8fd7c8aeca962e3cb8c906f328af69b061e0ff73
dnrgus1127 <dnrgus1127@naver.com>
2023-06-28 14:08:12 +0900

    FIX (trendingPost) : indicator 개수 코드 수정

--- src/components/mainPage/MainPagePostSlide.js
+++ src/components/mainPage/MainPagePostSlide.js
@@ -226,24 +226,27 @@ export default function MainPagePostSlide({ data }) {
             </svg>
           )}
         </button>
-        {indicatorList.map((i, idx) => (
-          <svg
-            key={idx + i}
-            className={idx + 1 === currentSlide ? "selected" : ""}
-            clipRule='evenodd'
-            fillRule='evenodd'
-            strokeLinejoin='round'
-            strokeMiterlimit='2'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z'
-              fillRule='nonzero'
-            />
-            <circle cx='11.998' cy='11.998' fillRule='nonzero' r='9.998' />
-          </svg>
-        ))}
+        {indicatorList.map((i, idx) => {
+          if (idx >= lastIdx) return <></>;
+          return (
+            <svg
+              key={idx + i}
+              className={idx + 1 === currentSlide ? "selected" : ""}
+              clipRule='evenodd'
+              fillRule='evenodd'
+              strokeLinejoin='round'
+              strokeMiterlimit='2'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <path
+                d='m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497 8.497-3.807 8.497-8.497-3.807-8.498-8.497-8.498z'
+                fillRule='nonzero'
+              />
+              <circle cx='11.998' cy='11.998' fillRule='nonzero' r='9.998' />
+            </svg>
+          );
+        })}
       </SlideIndicatorBox>
     </div>
   );
