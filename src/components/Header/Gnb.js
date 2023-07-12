657f8ffc3e79b589bb228fdde045d4e3f2005130
dnrgus1127 <dnrgus1127@naver.com>
2023-07-12 21:28:26 +0900

    File (Loading ) DIR 구조 변경

--- src/components/Header/Gnb.js
+++ src/components/Header/Gnb.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import NewPostBtn from "../NewPostBtn";
 
 import { useSelector } from "react-redux";
 import DarkModeBtn from "../common/button/DarkModeBtn";
+import NewPostBtn from "../WritePosts/NewPostBtn";
 
 const MenuSection = styled.div`
   display: flex;
