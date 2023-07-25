4f3e15dfd9542886844186fa20d035f9836c926c
dnrgus1127 <dnrgus1127@naver.com>
2023-07-25 16:31:21 +0900

    FEAT (댓글) : 댓글 작성 개선
    댓글 길이(줄)에 따라서 댓글 입력 창의 크기가 달라지도록 수정

--- src/components/posts/comments/CommentTA.js
+++ src/components/posts/comments/CommentTA.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const Container = styled.textarea`
+const CommentTextArea = styled.textarea`
   background-color: ${({ theme }) => theme.bgElement};
   border: 0.1px solid rgba(255, 255, 255, 0.2);
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
 
   border-radius: 2px;
   padding: 1.5rem 2.5rem;
-  font-size: 1.4rem;
+  font-size: 1.6rem;
   margin-bottom: 1rem;
 `;
 
 export default function CommentTA(props) {
-  return <Container {...props}></Container>;
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      // ref.current.style.height = 'auto';
+      let height = ref.current.scrollHeight; // 높이
+      ref.current.style.height = `${height}px`;
+    }
+  }, [props.value])
+
+
+
+  return <CommentTextArea ref={ref} rows={1} {...props}></CommentTextArea>;
 }
